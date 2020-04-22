d3f345e92d473a3fc5a5274fa1f62c424ef2ab42
Shardool Pathak <shardool@Shardools-MacBook-Pro.local>
2020-04-21 17:34:54 -0700

    Method skeletons for showing trend and log transform

--- gui.py
+++ gui.py
@@ -1,3 +1,7 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 class GUI:
     def __init__(self, data_object):
         self.dataController = data_object
@@ -6,3 +10,23 @@ class GUI:
         #show GUI
         print("test message for debugging")
         pass
+
+    def showTrend(self,x_vals,y_vals,x_label,y_label,title):
+        #Will graph the trend in cases for a given timeframe and given location
+        x = self.x_vals
+        y = self.y_vals
+        x_label = self.x_label
+        y_label = self.y_label
+        t = self.title
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(t)
+        plt.plot(x,y)  
+        plt.show()
+        return 
+
+    def showLogTransform(self,x_vals,y_vals,x_label,y_label,title):
+        '''Will plot a log transformation to present exponential 
+           trends for a give timeframe and given location'''
+        return
+

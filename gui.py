7b35155aab17b380b57eef73a698110548fc539b
Shardool Pathak <shardool@Shardools-MacBook-Pro.local>
2020-04-21 21:11:25 -0700

    method stub for map gui

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
+"""
+@author: Shardool
+"""
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import geopandas as gpd
+import pandas as pandas
 
 class GUI:
     def __init__(self, data_object):
@@ -10,6 +16,13 @@ class GUI:
         #show GUI
         print("test message for debugging")
         pass
+
+
+    def displayMap(data):
+        gdf = GeoDataFrame(df, crs=crs, geometry=geometry)
+        #TODO: display data on a map GUI and add slider 
+
+
 
     def showTrend(self,x_vals,y_vals,x_label,y_label,title):
         #Will graph the trend in cases for a given timeframe and given location
